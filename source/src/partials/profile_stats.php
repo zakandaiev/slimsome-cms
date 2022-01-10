2416e10763d70379073e5999238733076818f973
Oleksandr Zakandaiev <45367051+zakandaiev@users.noreply.github.com>
2022-01-10 16:27:25 +0200

    v1.1.4

--- source/src/partials/profile_stats.php
+++ source/src/partials/profile_stats.php
@@ -65,6 +65,7 @@ $earns_per_months_rub = $pdo->query("
           CROSS JOIN (SELECT 0 as a UNION ALL SELECT 1 UNION ALL SELECT 2 UNION ALL SELECT 3 UNION ALL SELECT 4 UNION ALL SELECT 5 UNION ALL SELECT 6 UNION ALL SELECT 7 UNION ALL SELECT 8 UNION ALL SELECT 9) as d
       ) tt
       WHERE tt.Date BETWEEN (SELECT MIN(cdate) FROM awesome_payments WHERE status=1 LIMIT 1) AND NOW()
+      GROUP BY year, month
       ORDER BY year, month
   ) t2 ON t2.month=t1.month AND t1.year=t2.year
   WHERE t2.month IS NOT NULL
